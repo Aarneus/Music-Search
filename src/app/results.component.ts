a7090c7ff5ba39fbe17213aa982d008bb361b7af
Aarneus <aarneus@gmail.com>
2017-04-26 20:59:38 +0300

    Parameterized the search result list

--- src/app/results.component.ts
+++ src/app/results.component.ts
@@ -9,51 +9,29 @@ import {Component, View} from 'angular2/core';
     template: `
         <div class="container results">
             <div class="row results-header">
-                <div class="col-xs-3">Track</div>
-                <div class="col-xs-3">Artists</div>
-                <div class="col-xs-3">Album</div>
+                <div class="col-xs-6">Name</div>
+                <div class="col-xs-2">Type</div>
+                <div class="col-xs-1">Tracks</div>
                 <div class="col-xs-3">Length</div>
             </div>
             
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
+            <div class="row" *ngFor="#row of results">
+                <div class="col-xs-6">{{row.name}}</div>
+                <div class="col-xs-2">{{row.type}}</div>
+                <div class="col-xs-1">{{row.tracks}}</div>
+                <div class="col-xs-3">{{row.length}}</div>
             </div>
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-                <div class="col-xs-3">????</div>
-            </div>
+            
         </div>
     `
 })
 
 export class ResultsComponent {
+    
+    // Results
+    private results = [
+        {name: "Johnny's Album", type: "Album", length: "1 hr 15 min", tracks: "12"},
+        {name: "Johnny's Album 2", type: "Album", length: "2 hr 5 min", tracks: "11"},
+        {name: "Johnny's Album 3: The Johnnybaloo", type: "Album", length: "24 hr", tracks: "120"}
+    ];
 }    
