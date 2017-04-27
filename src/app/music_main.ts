c57b637d692c94007dffe61c807a78989a82ae26
Aarneus <aarneus@gmail.com>
2017-04-27 15:31:19 +0300

    Enabled production mode

--- src/app/music_main.ts
+++ src/app/music_main.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {enableProdMode} from 'angular2/core';
 import {AppComponent} from './music_app.component';
 import {SearchService} from './search.service';
 
-//enableProdMode();
+enableProdMode();
 bootstrap(AppComponent, [SearchService]);
