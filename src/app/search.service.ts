8974ad835202b7731bf490a527c26915a9fee630
Aarneus <aarneus@gmail.com>
2017-04-27 11:26:31 +0300

    Integrated Spotify Web API

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -9,15 +9,31 @@ declare var SpotifyWebApi : any;
 // Handles communication between the search and results components and the Spotify Web API
 @Injectable()
 export class SearchService {
-
-    // Forwards search results
     private resultsSource = new Subject<SearchResult[]>();
+    private spotify : SpotifyWebApi;
     
+    constructor() {
+        this.spotify = new SpotifyWebApi();
+        
+        this.spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', 
+            function(err, data) {
+                if (err) console.error(err);
+                else console.log('Artist albums', data);
+            });
+
+    }
+
     
     
     // Performs a search from the Spotify Web API with the given terms
     public search(text: string, methods: string[]) {
-        alert(config.clientID);
+        // TODO
+        
+        var spotifyApi = new SpotifyWebApi();
+        
+        alert(text);
+        var testResults = [{name: "Rocketeer's Song", type: "Track", length: "3 min", tracks: "1"}];
+        this.sendResults(testResults);
     }
     
     // Sends an event with new results for ResultsComponents to display
